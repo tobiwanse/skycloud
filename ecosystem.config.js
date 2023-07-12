1ae7c9d83d39f0d4e2520121e75ea05aee4ff202
tobias <tobias@macbook-pro.local>
2023-07-12 14:05:48 +0000

    init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	apps: [ {
 		name: "skycloud",
-		script: "server/server.js",
+		script: "server/serverr.js",
 		watch: true,
 		watch_delay: 1000,
 		ignore_watch: [ "node_modules", "cumulus", ".git", "dist", ".nova" ],
