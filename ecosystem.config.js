d6884157f7fe7f2d36ce8bf768f75fb767530dd4
tobias <tobias@macbook-pro.local>
2023-07-12 13:16:52 +0000

    init

---
+++ ecosystem.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+	apps: [ {
+		name: "skycloud",
+		script: "./server/server.js",
+		watch: true,
+		watch_delay: 1000,
+		ignore_watch: [ "node_modules", "cumulus", ".git", "dist", ".nova" ],
+		env: {
+			NODE_ENV: 'development'
+		},
+		env_production: {
+			NODE_ENV: 'production'
+		}
+	} ],
+	deploy: {
+		development: {
+			key: "/Users/tobias/.ssh/id_rsa",
+			user: "macmini",
+			host: [ "mycloud.nu" ],
+			ssh_options: ["StrictHostKeyChecking=no", "ForwardAgent=yes"],
+			ref: "origin/main",
+			repo: "git@github.com:tobiwanse/skycloud-server.git",
+			path: "/Users/macmini/www/test.skycloud.nu",
+			//"post-deploy": "export PATH=$PATH:/usr/local/bin npm install && pm2 startOrRestart ecosystem.config.js --env development"
+			"post-deploy": "export PATH=$PATH:/usr/local/bin npm install && pm2 startOrRestart ecosystem.config.js --env development"
+		},
+		production: {
+			key: "/Users/tobias/.ssh/id_rsa",
+			user: "macmini",
+			host: [ "mycloud.nu" ],
+			ssh_options: "StrictHostKeyChecking=no",
+			ref: "origin/main",
+			repo: "git@github.com:tobiwanse/skycloud-server.git",
+			path: "/Users/macmini/www/test.skycloud.nu",
+			"post-deploy": 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
+		}
+	}
+}
