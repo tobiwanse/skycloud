cf3ab8f581f55e83bbb389e810f42cccb1f4580c
tobias <tobias@macbook-pro.local>
2023-07-13 07:43:05 +0000

    ddd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,12 @@
 module.exports = {
-	apps: [ {
-		name: "skycloud-dev",
-		script: "server.js",
-		watch: true,
-		watch_delay: 1000,
-		ignore_watch: [ "node_modules", "cumulus", ".git", "dist", ".nova" ],
-		env: {
-			NODE_ENV: 'development'
-		}
-	},{
-		name: "skycloud-prod",
+	apps: [{
+		name: "skycloud",
 		script: "server.js",
 		env_production: {
 			NODE_ENV: 'production'
 		}
 	} ],
 	deploy: {
-		development: {
-			key: "/Users/tobias/.ssh/id_rsa",
-			user: "macmini",
-			host: [ "mycloud.nu" ],
-			ssh_options: ["StrictHostKeyChecking=no"],
-			ref: "origin/main",
-			repo: "git@github.com:tobiwanse/skycloud.git",
-			path: "/Users/macmini/www/test.skycloud.nu",
-			"post-deploy": "export PATH=$PATH:/usr/local/bin && cd server && npm install && pm2 startOrRestart ../ecosystem.config.js --env development && pm2 log"
-		},
 		production: {
 			key: "/Users/tobias/.ssh/id_rsa",
 			user: "macmini",
@@ -34,7 +15,7 @@ module.exports = {
 			ref: "origin/main",
 			repo: "git@github.com:tobiwanse/skycloud.git",
 			path: "/Users/macmini/www/skycloud.nu",
-			//"post-deploy": "export PATH=$PATH:/usr/local/bin && cd server && npm install && pm2 startOrRestart ../ecosystem.config.js --env production"
+			"post-deploy": "export PATH=$PATH:/usr/local/bin && cd server && npm install && pm2 startOrRestart ../ecosystem.config.js --env production"
 		}
 	}
 }
