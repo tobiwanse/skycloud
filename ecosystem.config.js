6697f94e6d96656d8f1ded203e4ded397fe80cb2
tobias <tobias@macbook-pro.local>
2023-07-21 06:19:22 +0000

    commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	apps: [{
 		name: "skycloud",
 		script: "server.js",
+		cwd: "./server"
 		env_production: {
 			NODE_ENV: 'production'
 		}
@@ -15,7 +16,7 @@ module.exports = {
 			ref: "origin/main",
 			repo: "git@github.com:tobiwanse/skycloud.git",
 			path: "/Users/macmini/www/skycloud.nu",
-			"post-deploy": "export PATH=$PATH:/usr/local/bin && cd server && npm install && pm2 startOrRestart ../ecosystem.config.js --env production && pm2 save"
+			"post-deploy": "export PATH=$PATH:/usr/local/bin && npm install && pm2 startOrRestart ../ecosystem.config.js --env production"
 		}
 	}
 }
